cf16466467cd9053760e8e28990417a699bab613
Aryan Patil <79255053+Aryan-Patil@users.noreply.github.com>
2022-12-23 14:52:28 +0530

    Added bootstrap

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,8 @@ export default function About() {
                     <li>learing Automation Testing</li>
                 </ul>
             </div>
-
+            
+            <div className="fake">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam omnis ducimus recusandae placeat eum voluptatum earum laudantium sequi veritatis. Soluta sapiente magnam quaerat. Placeat temporibus quia ullam sapiente reiciendis eaque.</div>
         </div>
     )
 }
