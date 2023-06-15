a730ea9ead0f7174183bb737ba52d1d4621ef2c3
Aryan Patil <79255053+Aryan-Patil@users.noreply.github.com>
2023-06-15 22:57:02 +0530

    Portfolio design done

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,18 +1,94 @@
-import React from 'react'
-import Header from '../Components/Header'
-
+import React, { useState } from 'react';
+import Header from '../Components/Header';
+import './Css/Portfolio.css';
 export default function Portfolio() {
-  const allvis = ()=>{
-    
-  }
+  const [all, setAll] = useState(true);
+  const [sde, setSde] = useState(false);
+  const [webdev, setWebdev] = useState(false);
+  const [webdes, setWebdes] = useState(false);
+
+  const allvis = () => {
+    return (
+      <div>
+        All
+      </div>
+    );
+  };
+
+  const sdevis = () => {
+    return (
+      <div>
+        Software Development
+      </div>
+    );
+  };
+
+  const webdevvis = () => {
+    return (
+      <div>
+        Web Development
+      </div>
+    );
+  };
+
+  const webdesvis = () => {
+    return (
+      <div>
+        Web Design
+      </div>
+    );
+  };
+
+  const Output = () => {
+    if (sde) {
+      return sdevis();
+    } else if (webdev) {
+      return webdevvis();
+    } else if (webdes) {
+      return webdesvis();
+    } else if (all) {
+      return allvis();
+    }
+  };
+
   return (
     <div>
       <Header />
       <h1 className="heading-name">Portfolio</h1>
       <div className="heading-underline"></div>
 
-      <div className="fake">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam omnis ducimus recusandae placeat eum voluptatum earum laudantium sequi veritatis. Soluta sapiente magnam quaerat. Placeat temporibus quia ullam sapiente reiciendis eaque.</div>
+      <div className="">
+        <button className={all ? "projects-btn projects-btn-active": "projects-btn"}  onClick={() => {
+          setAll(true);
+          setSde(false);
+          setWebdes(false);
+          setWebdev(false);
+        }}>All</button>
+        <button className={webdev ? "projects-btn projects-btn-active": "projects-btn"} onClick={() => {
+          setAll(false);
+          setSde(false);
+          setWebdes(false);
+          setWebdev(true);
+        }}>Web Development</button>
+        <button className={webdes ? "projects-btn projects-btn-active": "projects-btn"} onClick={() => {
+          setAll(false);
+          setSde(false);
+          setWebdes(true);
+          setWebdev(false);
+        }}>Web Design</button>
+        <button className={sde ? "projects-btn projects-btn-active": "projects-btn"} onClick={() => {
+          setAll(false);
+          setSde(true);
+          setWebdes(false);
+          setWebdev(false);
+        }}>Software Development</button>
+      </div>
 
+      <Output />
+
+      <div className="fake">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam omnis ducimus recusandae placeat eum voluptatum earum laudantium sequi veritatis. Soluta sapiente magnam quaerat. Placeat temporibus quia ullam sapiente reiciendis eaque.
+      </div>
     </div>
-  )
+  );
 }
