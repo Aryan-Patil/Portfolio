e6c8c09ed0cc07d56059e85c9e812acd6a5070c2
Aryan Patil <79255053+Aryan-Patil@users.noreply.github.com>
2023-06-16 01:55:32 +0530

    Contact us section modified (more optimized)

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,73 +1,65 @@
-import React, { useState } from 'react'
-import Header from '../Components/Header'
+import React, { useState } from 'react';
+import Header from '../Components/Header';
 import { Player } from '@lottiefiles/react-lottie-player';
-import "./Css/Contact.css"
+import './Css/Contact.css';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    name: '',
+    content: '',
+  });
 
-  const [email, setEmail] = useState("")
-  const [msg1, setmsg1] = useState(false)
-  const [msg2, setmsg2] = useState(false)
-  const [msg3, setmsg3] = useState(false)
-  const [name, setname] = useState("")
-  const [content, setcontent] = useState("")
+  const updateFormValue = (key, value) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [key]: value,
+    }));
+  };
 
-  const emailValidation = (e) => {
-    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-    var emailValue = e.target.value
-    setEmail(emailValue)
-    if (email.match(pattern)) {
-      setmsg1(true);
-    } else {
-      setmsg1(false);
-    }
-  }
-  const nameVad = (e) => {
-    var namevalue = e.target.value
-    setname(namevalue)
-    if (name.length > 0) {
-      setmsg2(true)
-    }
-    else {
-      setmsg2(false)
-    }
-  }
+  const validateEmail = () => {
+    return formValues.email.match(/^[^ ]+@[^ ]+\.[a-z]{2,3}$/);
+  };
 
-  const bodyVad = (e) => {
-    var bodyvalue = e.target.value
-    setcontent(bodyvalue)
-    if (content.length > 0) {
-      setmsg3(true)
-    }
-    else {
-      setmsg3(false)
-    }
-  }
-  const formSubmit = (e) => {
+  const validateName = () => {
+    return formValues.name.length > 0;
+  };
+
+  const validateContent = () => {
+    return formValues.content.length > 0;
+  };
+
+  const isFormValid = validateEmail() && validateName() && validateContent();
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    var prams = {
-      name: name,
-      email: email,
-      content: content
-    }
+
+    const { email, name, content } = formValues;
+
+    const params = {
+      name,
+      email,
+      content,
+    };
+
     const serviceId = "service_hvfyfke";
-    const tempId = "template_8v2uhmd";
+    const templateId = "template_8v2uhmd";
     const userId = 'frdFq_AgCGZW4U4rM';
-    emailjs.send(serviceId, tempId, prams, userId).then(
-      res => {
+
+    emailjs
+      .send(serviceId, templateId, params, userId)
+      .then((res) => {
         alert('Thanks for contacting me!');
         console.log(res);
-        setEmail('');
-        setname('');
-        setcontent('');
-        setmsg1(false);
-        setmsg2(false);
-        setmsg3(false);
-      }).catch(err => console.log(err))
-  }
-
-
+        setFormValues({
+          email: '',
+          name: '',
+          content: '',
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
@@ -76,52 +68,73 @@ export default function Contact() {
       <div className="heading-underline fade-left"></div>
       <div className="Contact-card fade-in">
         <div className="Contact-animation">
-          <Player className='Animation'
+          <Player
+            className="Animation"
             autoplay
             loop
             src="https://assets6.lottiefiles.com/packages/lf20_t1yo4z4b.json"
           />
-
         </div>
-        <form className="Contact-body" onSubmit={formSubmit}>
-          <h3>Get in touch</h3><br />
+        <form className="Contact-body" onSubmit={handleSubmit}>
+          <h3>Get in touch</h3>
+          <br />
           <div className="Contact-name contact-body-info">
-            <span>Name </span><br />
-            <input className={name.length === 0 ?
-              "Width-input border-white" :
-              msg2 ?
-                "Width-input border-green" :
-                "Width-input border-red"
-            }
-              onChange={nameVad} required value={name} placeholder='Enter Name' type="text" name='username' />
+            <span>Name </span>
+            <br />
+            <input
+              className={`Width-input ${formValues.name.length === 0 ? 'border-white' : validateName() ? 'border-green' : 'border-red'}`}
+              onChange={(e) => updateFormValue('name', e.target.value)}
+              required
+              value={formValues.name}
+              placeholder="Enter Name"
+              type="text"
+              name="username"
+            />
           </div>
           <div className="Contact-email contact-body-info">
-            <span>Email </span><br />
-            <input className={email.length === 0 ?
-              "Width-input border-white" :
-              msg1 ?
-                "Width-input border-green" :
-                "Width-input border-red"
-            }
-              placeholder='Enter E-Mail' required onChange={emailValidation} value={email} type="email" name="Email" id="" />
+            <span>Email </span>
+            <br />
+            <input
+              className={`Width-input ${
+                formValues.email.length === 0 ? 'border-white' : validateEmail() ? 'border-green' : 'border-red'
+              }`}
+              placeholder="Enter E-Mail"
+              required
+              onChange={(e) => updateFormValue('email', e.target.value)}
+              value={formValues.email}
+              type="email"
+              name="Email"
+              id=""
+            />
           </div>
           <div className="Contact-question contact-body-info">
-            <span>Message </span><br />
-            <textarea className={content.length === 0 ?
-              "Width-input border-white" :
-              msg3 ?
-                "Width-input border-green" :
-                "Width-input border-red"
-            }
-              onChange={bodyVad} value={content} required
-              placeholder='Enter Message' name="message" id="" rows="7"></textarea>
+            <span>Message </span>
+            <br />
+            <textarea
+              className={`Width-input ${
+                formValues.content.length === 0 ? 'border-white' : validateContent() ? 'border-green' : 'border-red'
+              }`}
+              onChange={(e) => updateFormValue('content', e.target.value)}
+              value={formValues.content}
+              required
+              placeholder="Enter Message"
+              name="message"
+              id=""
+              rows="7"
+            ></textarea>
           </div>
-          <input type="submit" className={msg1 ? msg2 ? msg3 ? "button-main button-active" : "button-disabled" : "button-disabled" : "button-disabled"} value="Submit" />
+          <input
+            type="submit"
+            className={`button-main ${isFormValid ? 'button-active' : 'button-disabled'}`}
+            value="Submit"
+          />
         </form>
       </div>
       <div className="fake">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consectetur rerum minus, accusantium quas commodi accusamus nobis consequatur sapiente, unde quae, corporis tempore recusandae eligendi repudiandae beatae tenetur nihil adipisci.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consectetur rerum minus, accusantium quas commodi
+        accusamus nobis consequatur sapiente, unde quae, corporis tempore recusandae eligendi repudiandae beatae tenetur nihil
+        adipisci.
       </div>
     </div>
-  )
+  );
 }
